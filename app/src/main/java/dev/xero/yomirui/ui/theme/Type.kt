6535d650874f71cf8cf255760781698a5fca94d4
Xero kun <elo48858@gmail.com>
2023-03-02 15:37:30 +0100

    `✨UPDATE` Updated Typography

--- app/src/main/java/dev/xero/yomirui/ui/theme/Type.kt
+++ app/src/main/java/dev/xero/yomirui/ui/theme/Type.kt
@@ -2,27 +2,61 @@ package dev.xero.yomirui.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import dev.xero.yomirui.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-	body1 = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.Normal,
-		fontSize = 16.sp
+val Inter = FontFamily(
+	Font(
+		resId = R.font.inter_regular,
+		FontWeight.Normal
+	),
+
+	Font(
+		resId = R.font.inter_bold,
+		FontWeight.Bold
 	)
-	/* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+)
+
+val OutFit = FontFamily(
+	Font(
+		resId = R.font.outfit_bold,
+		FontWeight.Bold
+	),
+)
+
+val Typography = Typography(
+	h3 = TextStyle(
+		fontFamily = OutFit,
+		fontWeight = FontWeight.Bold,
+		fontSize = 24.sp
+	),
+
+	h4 = TextStyle(
+		fontFamily = Inter,
+		fontWeight = FontWeight.Bold,
+		fontSize = 14.sp
+	),
+
+	h6 = TextStyle(
+		fontFamily = Inter,
+		fontWeight = FontWeight.Bold,
+		fontSize = 12.sp
+	),
+
+	caption = TextStyle(
+		fontFamily = OutFit,
+		fontWeight = FontWeight.Bold,
+		fontSize = 10.sp
+	),
+
+	body1 = TextStyle(
+		fontFamily = Inter,
+		fontWeight = FontWeight.Normal,
+		fontSize = 14.sp
+	)
+
+
 )
