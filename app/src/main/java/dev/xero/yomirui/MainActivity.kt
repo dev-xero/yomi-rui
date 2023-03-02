0cc7fae7ad51e59617ce617a6593c4d1d9413f01
Xero kun <elo48858@gmail.com>
2023-03-02 15:49:46 +0100

    `🐛BUG FIX` Fixed Error Screen Alignment

--- app/src/main/java/dev/xero/yomirui/MainActivity.kt
+++ app/src/main/java/dev/xero/yomirui/MainActivity.kt
@@ -3,6 +3,8 @@ package dev.xero.yomirui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dev.xero.yomirui.ui.YomiRuiApp
 import dev.xero.yomirui.ui.theme.YomiRuiTheme
--- app/src/main/java/dev/xero/yomirui/ui/screens/ErrorScreen.kt
+++ app/src/main/java/dev/xero/yomirui/ui/screens/ErrorScreen.kt
@@ -2,10 +2,7 @@ package dev.xero.yomirui.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -13,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.xero.yomirui.R
@@ -27,12 +25,12 @@ fun ErrorScreen(
 	Box(
 		modifier = modifier
 			.fillMaxSize()
+			.padding(horizontal = 12.dp)
 			.background(color = Background),
 		contentAlignment = Alignment.Center
 	) {
 
 		Column(
-			modifier = Modifier.fillMaxSize(),
 			horizontalAlignment = Alignment.CenterHorizontally,
 			verticalArrangement = Arrangement.spacedBy(24.dp)
 		) {
@@ -44,13 +42,15 @@ fun ErrorScreen(
 			Text(
 				text = stringResource(id = R.string.unable_to_connect),
 				style = MaterialTheme.typography.h3,
-				color = White
+				color = White,
+				textAlign = TextAlign.Center
 			)
 			
 			Text(
 				text = stringResource(id = R.string.check_connection),
 				style = MaterialTheme.typography.h4,
-				color = Purple100
+				color = Purple100,
+				textAlign = TextAlign.Center
 			)
 		}
 
