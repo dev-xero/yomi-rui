e7b7d1f292b3a6d29fca950695e95b4c9812f54c
Xero kun <elo48858@gmail.com>
2023-03-03 14:11:59 +0100

    `✨UPDATE` Setup MangaInfo Data Model

--- app/src/main/java/dev/xero/yomirui/model/MangaInfo.kt
+++ app/src/main/java/dev/xero/yomirui/model/MangaInfo.kt
@@ -1,7 +1,14 @@
 package dev.xero.yomirui.model
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class MangaInfo (
-	val name: List<String>
+	val images: List<List<String>>,
+	val title: String,
+	@SerialName("title_japanese")
+	val japaneseTitle: String,
+	val published: List<List<List<String>>>,
+	val synopsis: String,
+	val chapters: Int
 )
