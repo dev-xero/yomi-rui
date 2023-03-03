3cf1dbf8f9863f427b32321a50fba2e29305f1ba
Xero kun <elo48858@gmail.com>
2023-03-03 14:54:16 +0100

    `✨UPDATE` Setup View Model

--- app/src/main/java/dev/xero/yomirui/ui/YomiRuiApp.kt
+++ app/src/main/java/dev/xero/yomirui/ui/YomiRuiApp.kt
@@ -2,13 +2,17 @@ package dev.xero.yomirui.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.xero.yomirui.ui.screens.ErrorScreen
 import dev.xero.yomirui.ui.screens.HomeScreen
 import dev.xero.yomirui.ui.screens.LoadingScreen
+import dev.xero.yomirui.ui.viewmodel.YomiViewModel
 
 @Composable
 fun YomiRuiApp(
 	modifier: Modifier = Modifier
 ) {
+	val viewModel: YomiViewModel = viewModel(factory = YomiViewModel.Factory)
+
 	HomeScreen(modifier = modifier)
 }
--- app/src/main/java/dev/xero/yomirui/ui/viewmodel/YomiViewModel.kt
+++ app/src/main/java/dev/xero/yomirui/ui/viewmodel/YomiViewModel.kt
@@ -1,12 +1,41 @@
 package dev.xero.yomirui.ui.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import dev.xero.yomirui.YomiRuiApplication
+import dev.xero.yomirui.repository.YomiRuiRepositoryInterface
+import kotlinx.coroutines.launch
+import java.io.IOException
 
-class YomiViewModel : ViewModel() {
-	sealed interface AppDataState {
-		object Loading: AppDataState
+class YomiViewModel(
+	private val repository: YomiRuiRepositoryInterface
+) : ViewModel() {
 
-		object Failure: AppDataState
+	private val tag = "APP"
+
+	init {
+		viewModelScope.launch {
+			try {
+				Log.d(tag, repository.getTopManga().toString())
+			} catch (e: IOException) {
+				Log.d(tag, "FAIL")
+			}
+		}
+	}
+
+	// Factory for the viewModel
+	companion object {
+		val Factory: ViewModelProvider.Factory = viewModelFactory {
+			initializer {
+				val application = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as YomiRuiApplication)
+				val repository = application.container.repository
+				YomiViewModel(repository)
+			}
+		}
 	}
 
 }
